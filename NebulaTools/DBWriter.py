f4a2fdf2370f0adcdfde5a6fdf0ddd411e614d14
Lian Xu <lxu4@wework.local>
2019-12-30 14:01:20 +0800

    PMRDeserializer Update

---
+++ NebulaTools/DBWriter.py
@@ -0,0 +1,7 @@
+from utils.PMRDeserializer import PMRDeserializer
+from utils.PMRAPIWrapper import PMRAPIWrapper
+
+
+def main(json):
+    wrapper = PMRAPIWrapper()
+    deserializer = PMRDeserializer(json_file)
