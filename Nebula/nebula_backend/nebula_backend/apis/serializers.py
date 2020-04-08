2f2b1e8504a508f4257ead4b644fffd6d7a882a9
xzhang-wework <xin.zhang4@wework.com>
2020-04-08 18:00:08 +0800

    JSONAPI

--- Nebula/nebula_backend/nebula_backend/apis/serializers.py
+++ Nebula/nebula_backend/nebula_backend/apis/serializers.py
@@ -7,12 +7,26 @@ class RoomSerializer(serializers.ModelSerializer, gs.GeoModelSerializer):
     class Meta:
         model = Room
         fields = "__all__"
+        read_only_fields = (
+            "room_id",
+            "room_revit_id",
+            "floor_id",
+            "level_revit_id"
+        )
 
 
 class FloorSerializer(serializers.ModelSerializer):
+    included_serializers = {
+        "rooms":"nebula_backend.apis.serializers.RoomSerializer"
+    }
     class Meta:
         model = Floor
-        fields = ("floor_name", "elevation", "project_id")
+        fields = "__all__"
+        read_only_fields = (
+            "project_id",
+            "level_revit_id",
+            "floor_id"
+        )
 
 
 class NestFloorSerializer(serializers.ModelSerializer):
@@ -22,6 +36,39 @@ class NestFloorSerializer(serializers.ModelSerializer):
 
 
 class ProjectSerializer(serializers.ModelSerializer):
+
+    included_serializers = {
+        "floors":"nebula_backend.apis.serializers.NestFloorSerializer"
+    }
+
     class Meta:
         model = Project
-        fields = "__all__"
+        fields = [
+            "stargate_id",
+            "pmr_repository_id",
+            "project_name",
+            "building_name",
+            "business_line",
+            "landlord_id",
+            "project_address_point",
+            "project_address_en",
+            "project_address_local",
+            "project_market",
+            "project_city",
+            "project_status",
+            "usf_per_desk",
+            "pmr_branch_id",
+            "average_office_desk_count",
+            "template_version",
+            "revit_file_path",
+            "floors"
+
+        ]
+        read_only_fields = (
+            "floors",
+            "project_id",
+            "pmr_branch_id",
+            "pmr_repository_id",
+            "stargate_id"
+        )
+
