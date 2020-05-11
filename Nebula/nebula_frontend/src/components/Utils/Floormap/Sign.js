d312aee4d5db7cb7bce59d064a06724259ce2c90
bixaww <48235496+bxiaww@users.noreply.github.com>
2020-05-11 10:23:33 +0800

    renderer update
    1. add gravity center point
    2. add icon sprite for WE, WASH, OPERATE rooms

--- Nebula/nebula_frontend/src/components/Utils/Floormap/Sign.js
+++ Nebula/nebula_frontend/src/components/Utils/Floormap/Sign.js
@@ -1,23 +1,68 @@
 import * as THREE from "three";
 
-function SignGenerator(mesh) {
+function SignGenerator(mesh, gravityCenter) {
   // calculate the location of this text
   mesh.geometry.computeBoundingBox();
-  const bb3 = mesh.geometry.boundingBox;
-  const centroidX = (bb3.max.x + bb3.min.x) / 2;
-  const centroidY = (bb3.max.y + bb3.min.y) / 2;
+  // const bb3 = mesh.geometry.boundingBox;
+  // const centroidX = (bb3.max.x + bb3.min.x) / 2;
+  // const centroidY = (bb3.max.y + bb3.min.y) / 2;
+  const centroidX = gravityCenter.gravityX;
+  const centroidY = gravityCenter.gravityY;
 
   var img_path;
   switch (mesh.roomName) {
     case "MEN WC":
-      img_path = "https://api.c3plus.top/media/images/origin/man.png";
+      img_path = "https://api.c3plus.top/media/images/origin/wc_m2x.png";
       break;
+    case "W RESTROOM":
     case "WOMEN WC":
-      img_path = "https://api.c3plus.top/media/images/origin/woman.png";
+      img_path = "https://api.c3plus.top/media/images/origin/wc_f2x.png";
       break;
+    case "RESTROOM":
     case "UNISEX":
-      img_path =
-        "https://api.c3plus.top/media/images/origin/man-woman_KvpnENb.png";
+      img_path = "https://api.c3plus.top/media/images/origin/wc_u2x.png";
+      break;
+    case "VIP Lounge":
+    case "MINI LOUNGE":
+    case "LOUNGE":
+      img_path = "https://api.c3plus.top/media/images/origin/lounge.png";
+      break;
+    case "IT":
+      img_path = "https://api.c3plus.top/media/images/origin/IT_room2x.png";
+      break;
+    case "MAIL":
+      img_path = "https://api.c3plus.top/media/images/origin/mail_room2x.png";
+      break;
+    case "PHONE BOOTH":
+      img_path = "https://api.c3plus.top/media/images/origin/phone_booth2x.png";
+      break;
+    case "Cafe":
+    case "PANTRY":
+      img_path = "https://api.c3plus.top/media/images/origin/pantry2x.png";
+      break;
+    case "PRINTER":
+      img_path = "https://api.c3plus.top/media/images/origin/printer2x.png";
+      break;
+    case "STOR":
+      img_path = "https://api.c3plus.top/media/images/origin/storage2x.png";
+      break;
+    case "STAIR FOYER":
+      img_path = "https://api.c3plus.top/media/images/origin/stair.png";
+      break;
+    case "MOTHER RM":
+      img_path = "https://api.c3plus.top/media/images/origin/motherroom2x.png";
+      break;
+    case "MOP":
+      img_path = "https://api.c3plus.top/media/images/origin/maintenance2x.png";
+      break;
+    case "MECH":
+      img_path = "https://api.c3plus.top/media/images/origin/mechanical2x.png";
+      break;
+    case "ADA":
+      img_path = "https://api.c3plus.top/media/images/origin/wc_ada.png";
+      break;
+    case "COMMUNITY BAR":
+      img_path = "https://api.c3plus.top/media/images/origin/we_reception.png";
       break;
     default:
       return new THREE.Object3D();
@@ -27,7 +72,7 @@ function SignGenerator(mesh) {
   var spriteMaterial = new THREE.SpriteMaterial({ map: spriteMap });
   var sprite = new THREE.Sprite(spriteMaterial);
 
-  let scalar = 900;
+  let scalar = 1000;
   sprite.scale.multiplyScalar(scalar);
   sprite.position.set(centroidX, centroidY, 600);
 
