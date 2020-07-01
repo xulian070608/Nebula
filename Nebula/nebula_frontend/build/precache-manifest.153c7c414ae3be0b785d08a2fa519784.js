5184c36baed6c9cc5eba53cacff6bd6d15384d99
Baangyuan <bangyuan.xia@wework.com>
2020-07-01 11:44:48 +0800

    build update

---
+++ Nebula/nebula_frontend/build/precache-manifest.153c7c414ae3be0b785d08a2fa519784.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a07e8c68ec868aa2c798b54589c56c55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d43622c9fda4a7445082",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d43622c9fda4a7445082",
+    "url": "/static/js/2.5564b9fd.chunk.js"
+  },
+  {
+    "revision": "c7d31ffdb8812f3e47f78812e84a2579",
+    "url": "/static/js/2.5564b9fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62a83d9ad1371ef772c1",
+    "url": "/static/js/main.fb704fb6.chunk.js"
+  },
+  {
+    "revision": "7a008b5c3a6c3fefae51",
+    "url": "/static/js/runtime-main.18445cb3.js"
+  }
+]);
